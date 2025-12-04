6a8dc0498ee33a63c62305cd1b3a6df5c5ec90ad
elo-lvrz <leoaurelioalvarez@gmail.com>
2025-12-04 19:10:36 +0100

    Create route.ts

---
+++ src/app/logout/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { createSupabaseServer } from "@/lib/supabase-server";
+
+export async function GET() {
+  const supabase = createSupabaseServer();
+
+  await supabase.auth.signOut();
+
+  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_SITE_URL));
+}

efa47f55f462963f86c67c7eb6f90e1c0767f66e
elo-lvrz <leoaurelioalvarez@gmail.com>
2025-12-04 18:42:35 +0100

    Update supabase-browser.ts

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -1,7 +1,4 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 
 export const createBrowserSupabaseClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  createPagesBrowserClient();
